c564412f128fffd36a5e4a502613e4bffa171e40
Celalettin Turgut <celalettin.turgut@hotmail.com>
2021-12-24 01:15:16 +0100

    get and post request

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
 
 const app = express();
 const PORT = 8080;
 
+//start the database
+mongoose.connect(
+  "mongodb://localhost/clean-blog",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, data) => {
+    if (!err) console.log("Database baglantisi basari ile saglandi");
+  }
+);
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log("yeni istek");
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", { posts });
 });
 
 app.get("/about", (req, res) => {
@@ -19,6 +35,16 @@ app.get("/about", (req, res) => {
 app.get("/add", (req, res) => {
   console.log("yeni istek");
   res.render("add_post");
+});
+
+app.post("/add_post", async (req, res) => {
+  const post = Post.create(req.body);
+  res.redirect("/");
+});
+
+app.get("/post", (req, res) => {
+  console.log("yeni istek");
+  res.render("post");
 });
 
 app.listen(PORT, () => {
